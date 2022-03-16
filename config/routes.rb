c3f5924fad68c1307d155b56aaefc7436e8cb460
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-03-16 14:54:25 -0700

    commit tree cb4110fe2629f451dccdbd171efcb5be7c7e3b9b

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :dog_houses, only: [:show]
+  resources :dog_houses, only: [:show] do
+    #nested resource for reviews, which dog houses have many of
+    resources :reviews, only: [:show, :index] #will need to def these route methods in reviews_controller
+  end
   resources :reviews, only: [:show, :index, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
